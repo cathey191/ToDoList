3bdbebab98fe8cf724bcdb4623ed7c365e76fc34
Catherine <cathey191@hotmail.com>
2018-05-22 17:13:29 +1200

    Adding items, checkbox needs tlc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
-
-	$(".btn").click(function(){
-		var classCheck = document.querySelector('i')
-		if (this.innerHTML == '<i class="fas fa-check"></i>') {
-			this.innerHTML = '';
-		} else {
-			this.innerHTML = '<i class="fas fa-check"></i>';
-		}
-	});
+	(function() {
+		var inputSection = document.querySelector('#inputSection')
 
 
+		$(".btn").click(function(){
+			if (this.innerHTML == '<i class="fas fa-check"></i>') {
+				this.innerHTML = '';
+			} else {
+				this.innerHTML = '<i class="fas fa-check"></i>';
+			}
+		});
+
+
+		document.querySelector('input').addEventListener('keypress', function (e) {
+    	var key = e.which || e.keyCode;
+    	if (key === 13) {
+				var text = document.querySelector('input').value;
+				inputSection.insertAdjacentHTML('beforebegin', '<div class="input-group borderBottom" id="inputSection">'+'<div class="input-group-prepend">'+'<button class="btn btn-outline-secondary checkWidth" type="button">'+'</button>'+'</div>'+'<p class="form-control transparent" aria-label="Text input with checkbox" aria-describedby="basic-addon1">'+text+'</p>'+'</div>')
+				document.querySelector('input').value = '';
+			}
+		});
+
+
+	}());
 });
