7198594ae91d9174cba26da55efdf35a4e1d93c9
Catherine <cathey191@hotmail.com>
2018-05-22 14:24:17 +1200

    Styled checkbox working with jQuery

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(".btn").click(function(){
+		if (this.innerHTML == '<i class="far fa-square"></i>') {
+			this.innerHTML = '<i class="far fa-check-square"></i>';
+		} else {
+			this.innerHTML = '<i class="far fa-check-square"></i>';
+		}
+	});
+
+
+});
