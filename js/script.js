19246b2f82f75749f7d0818dce7d217e8e357ab2
cathey191 <cathey191@hotmail.com>
2018-05-23 09:27:20 +1200

    Remove Items

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 	(function() {
-		var inputSection = document.querySelector('#inputSection')
 
 
-		$(document).on('click', '.btn', function(){
+		$(document).on('click', '.tick', function(){
 			if (this.innerHTML == '<i class="fas fa-check"></i>') {
 				this.innerHTML = '';
 			} else {
@@ -16,17 +15,27 @@ $(document).ready(function(){
     	var key = e.which || e.keyCode;
     	if (key === 13) {
 				var text = document.querySelector('input').value;
-				var newLine = '<div class="input-group borderBottom" id="inputSection">';
+				var newLine = '<div class="input-group borderBottom">';
 				newLine += '		<div class="input-group-prepend">';
-				newLine += '			<button class="btn btn-outline-secondary checkWidth" type="button"></button>';
+				newLine += '			<button class="btn btn-outline-secondary checkWidth tick" type="button"></button>';
 				newLine += '		</div>';
 				newLine += '		<p class="form-control transparent">';
 				newLine += 				text;
-				newLine += '		</p></div>';
+				newLine += '		</p>';
+				newLine += '		<div class="input-group-append">'
+				newLine += '			<button class="btn btn-outline-secondary checkWidth" type="button">'
+				newLine += '				<i class="fas fa-times"></i>'
+				newLine += '			</button>'
+				newLine += '		</div>'
 				newLine += '	</div>'
 				inputSection.insertAdjacentHTML('beforebegin', newLine)
 				document.querySelector('input').value = '';
 			}
+		});
+
+		$(document).on('click', '.fa-times', function(){
+			var perant = this.parentNode.parentNode.parentNode
+			perant.outerHTML = '';
 		});
 
 
