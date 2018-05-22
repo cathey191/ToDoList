8dd610028e2c0ab6924f6b633df8700fda1e65cc
Catherine <cathey191@hotmail.com>
2018-05-22 16:14:49 +1200

    Removed unneeded code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// (function() {
-// 	var changeButton = document.querySelectorAll('.btn');
-//
-//
-// 	changeButton.addEventListener('click', function (){
-// 		var classCheck = document.querySelector('i')
-// 		if (this.innerHTML == '<i class="fas fa-check"></i>') {
-// 			this.innerHTML = '';
-// 		} else {
-// 			this.innerHTML = '<i class="fas fa-check"></i>';
-// 		}
-// 	}, false);
-//
-//
-// }());
-
 $(document).ready(function(){
 
 	$(".btn").click(function(){
